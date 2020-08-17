fc2cc6e99b34cccccbefad286efd84f1ee2ce015
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-17 11:49:08 +0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a,b,c;
+
+	printf("Type three letters: ");
+	a = getchar();
+	putchar(a);
+	b = getchar();
+	putchar(b);
+	c = getchar();
+	putchar(c);
+
+	_getch();
+	return(0);
+}
+
